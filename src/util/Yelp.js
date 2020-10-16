f25688a208a3564e26c15b9c91fb548d8fb6eed1
John Parrett <johnparrett@Johns-MBP.attlocal.net>
2020-10-16 02:59:59 -0400

    fourth and final push

---
+++ src/util/Yelp.js
@@ -0,0 +1,34 @@
+const apiKey = "Ez9pOggHvDbpUjvcMxDotTXkVnPNDxYy6lx_HvW2SS0FnBw-FWNCV6MJ6A7egObpXh_Y-ODlzBqkCwjqOwbxNc9LWtJjcmtKwN0qjur1BkvjkkDttpuvSStDVJlBX3Yx";
+
+const Yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map((business) => {
+                    console.log(business);
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count,
+                    };
+                });
+            }
+        })
+        
+    }
+}
+
+export default Yelp;
